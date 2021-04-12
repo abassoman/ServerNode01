32bf6fcbfa8193b3c2834f7bb80b1b4728404730
Alan Basso <alan.m.basso@gmail.com>
2021-04-11 22:20:24 -0400

    se actualiza proyecto

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import dotenv from "dotenv";
 import express from "express";
 import path from "path";
 import * as shell from "shelljs";
+import bodyParser from "body-parser";
+
 
 console.log('Se inica proyecto: Proyecto Server');
 
 dotenv.config();
 
 const app = express();
+const router = express.Router();
 const port = process.env.SERVER_PORT;
 
 shell.cp( "-R", "src/views", "dist/" );
@@ -18,6 +21,26 @@ app.set( "view engine", "ejs" );
 app.get( "/", ( req, res ) => {
     res.render( "index" );
 } );
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.post('/ejemplo',(request,response) => {
+    //code to perform particular action.
+    //To access POST variable use req.body()methods.
+    console.log(request.body);
+
+    let nombre = request.body.Nombre;
+    if(nombre=="Alan"){
+        response.status(204).json({Mensaje:'Tengo un mensaje vacio'});
+    } else{
+        response.status(200).json({Mensaje:'hola: '+ nombre});
+    }
+    return;
+});
+
+// add router in the Express app.
+app.use("/", router);
 
 app.listen( port, () => {
     console.log(  );
